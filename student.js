49fee89b5491d1e8d6eeb75c3062ba1d493f26b4
Laith Zayed <cenglaithzayed@gmail.com>
2020-10-19 13:32:41 +0300

    Student & Teacher Exercise

---
+++ student.js
@@ -0,0 +1,18 @@
+class student{
+    constructor(Id, Name,  Email, phone){ 
+        this.Id=Id;
+        this.Name=Name;
+        this.Email-Email;
+        this.phone=phone;
+    }
+    start(){
+        console.log(name.toString());
+       
+    }
+    start2(){
+        console.log(phone);
+        
+    }
+}
+
+let student1= new student('123443','sara','sara@orange.com','0777777777');
---
+++ teacher.js
@@ -0,0 +1,15 @@
+class teacher extends student{
+    constructor(Id, Name, Email, Mobile, salary, subjects){
+        super(Id, Name, Email, Mobile);
+        this.salary=salary;
+        this.subject=subject;
+        
+    }
+    end(){
+        console.log(salary.toString());
+        console.log(subject);
+
+        }
+}
+    
+let teacher1= new student('96432','sadi','sadi@orange.com','0777788888','800','[English, Arabic, Math, Science]');
